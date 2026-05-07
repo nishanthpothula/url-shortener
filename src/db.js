5230f1e607123d2bc1ad721fdc8081d60828f895
nishanth <nishanth.r7@gmail.com>
2026-05-06 22:26:43 -0700

    add pool timeout

--- src/db.js
+++ src/db.js
@@ -3,6 +3,8 @@ const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   max: 50,
+  connectionTimeoutMillis: 3000,
+  idleTimeoutMillis: 30000,
 });
 
 pool.on('error', (err) => {
